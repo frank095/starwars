afc787d1593b579f95f7dffedbd3ff613e1a2731
Frank <francois-ringuette@hotmail.com>
2020-08-18 18:57:38 -0400

    Tutorial #4

--- src/components/People.js
+++ src/components/People.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useQuery } from 'react-query';
 import Person from './Person';
 
-const fetchPeople = async () => {
-    const res = await fetch('http://swapi.dev/api/people/');
+const fetchPeople = async (key, greetings, page) => {
+    const res = await fetch(`http://swapi.dev/api/people/?page=${page}`);
     return res.json();
 }
 
 const People = () => {
-    const { data, status } = useQuery('people', fetchPeople);
+    const [ page, setPage ] = useState(1);
+    const { data, status } = useQuery(['people', 'SUP MOFOS', page], fetchPeople);
 
     console.log(data);
     return (
         <div>
             <h2>People</h2>
+
+            <button onClick={() => setPage(1)}>Page 1</button>
+            <button onClick={() => setPage(2)}>Page 2</button>
+            <button onClick={() => setPage(3)}>Page 3</button>
+
             {status === 'success' && (
                 <div>
                     {data.results.map(person => <Person key={person.name} person={person} />)}
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-    const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (key, greetings, page) => {
+    console.log(greetings);
+    const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
     return res.json();
 }
 
 const Planets = () => {
-    const { data, status } = useQuery('planets', fetchPlanets, {
+    const [ page, setPage ] = useState(1);
+    const { data, status } = useQuery(['planets', 'HELLO MOFOS', page], fetchPlanets, {
         staleTime: 0,
         cacheTime: 10,
         onSuccess: () => console.log('data fetched with no problemo.'),
@@ -18,6 +20,11 @@ const Planets = () => {
     return (
         <div>
             <h2>Planets</h2>
+
+            <button onClick={() => setPage(1)}>Page 1</button>
+            <button onClick={() => setPage(2)}>Page 2</button>
+            <button onClick={() => setPage(3)}>Page 3</button>
+
             {status === 'success' && (
                 <div>
                     {data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
