212d5e41cb53f9a45744efd4ca6ee4dd42a4ebb1
Frank <francois-ringuette@hotmail.com>
2020-08-18 19:15:56 -0400

    Tutorial #5

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,34 +1,39 @@
 import React, {useState} from 'react';
-import { useQuery } from 'react-query';
+import { usePaginatedQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async (key, greetings, page) => {
-    console.log(greetings);
+const fetchPlanets = async (key, page) => {
     const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
     return res.json();
 }
 
 const Planets = () => {
     const [ page, setPage ] = useState(1);
-    const { data, status } = useQuery(['planets', 'HELLO MOFOS', page], fetchPlanets, {
-        staleTime: 0,
-        cacheTime: 10,
-        onSuccess: () => console.log('data fetched with no problemo.'),
-    });
+    const { 
+        resolvedData,
+        latestData,
+        status 
+    } = usePaginatedQuery(['planets', page], fetchPlanets);
 
-    console.log(data);
     return (
         <div>
             <h2>Planets</h2>
 
-            <button onClick={() => setPage(1)}>Page 1</button>
-            <button onClick={() => setPage(2)}>Page 2</button>
-            <button onClick={() => setPage(3)}>Page 3</button>
-
             {status === 'success' && (
-                <div>
-                    {data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
-                </div>
+                <React.Fragment>
+                    <button 
+                        onClick={() => setPage(old => Math.max(old - 1, 1))}
+                        disabled={page === 1}
+                        >Previous Page</button>
+                    <span>{ page }</span>
+                    <button
+                        onClick={() => setPage(old => (!latestData || !latestData.next ? old : old + 1))}
+                        disabled={!latestData || !latestData.next}>
+                        Next Page</button>
+                    <div>
+                        {resolvedData.results.map(planet => <Planet key={planet.name} planet={planet} />)}
+                    </div>
+                </React.Fragment>
             )}
             {status === 'loading' && (
                 <div>LOADING DATA</div>
